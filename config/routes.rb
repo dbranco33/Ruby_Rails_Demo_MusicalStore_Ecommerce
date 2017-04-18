540011d4e3ada36ba4a4318a8b00023af44aa254
Daniel <vagrant@Base-Box>
2017-04-18 05:47:35 +0000

    Create partias for product's views. Define routes.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,30 @@ Rails.application.routes.draw do
   get 'product/index'
   get 'product/show'
   get 'welcome/index'
+
+  # Define the domain root
   root 'welcome#index'
+
+  resources 'product', only: [:index, :show, :acoustic_category]
+
+  # Routes for the pages that show products according their category
+  get 'products_acoustic' => 'product#acoustic_category',  as: 'acoustic_products'
+  get 'products_electric' => 'product#electric_category',  as: 'electric_products'
+  get 'products_bass'     => 'product#bass_category',      as: 'bass_products'
+  get 'products_amps'     => 'product#amps_category',      as: 'amps_products'
+  get 'products_acessory' => 'product#acessory_category',  as: 'acessory_products'
+  get 'products_other'    => 'product#other_category',     as: 'other_products'
+
+  # Routes for the pages that show products with the 'status' attribute equal to 'sale' and 'new'
+  get 'products_sale' => 'product#sale_products',  as: 'sale_products'
+  get 'products_new' => 'product#new_products',  as: 'new_products'
+
+  # Routes for the 'about' and 'contact' pages
+  resources 'about', only:[:about_us]
+  resources 'contact', only:[:contact_us]
+
+  # Route to the single keyword search
+  get 'search_results' => 'product#search_results', as: 'search_results'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
